dddf4cddcc486d866e128b5709374b99efeb0de8
Antoni05634 <antoniogsmac@gmail.com>
2014-04-24 09:22:07 -0300

    enviando exercicio complementares para o servidor

---
+++ exercicio7/exercicio8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    class exercicio8
+    {
+        public static int A, B, C, somaAB;
+        static void Mai786(string[] args)
+        {
+            Console.WriteLine("informe um valor:");
+            A = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
