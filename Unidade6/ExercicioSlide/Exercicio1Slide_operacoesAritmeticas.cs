a90ed8967747399f74052db6c3530bc57c315e62
Antoni05634 <antoniogsmac@gmail.com>
2014-04-30 11:56:16 -0300

    nao terminei

--- Unidade6/ExercicioSlide/Exercicio1Slide_operacoesAritmeticas.cs
+++ Unidade6/ExercicioSlide/Exercicio1Slide_operacoesAritmeticas.cs
@@ -8,5 +8,17 @@ namespace Unidade6.ExercicioSlide
 {
     class Exercicio1Slide_operacoesAritmeticas
     {
+        
+        static void Main(string[] args)
+        {
+             double a = 5;
+            double b = 2;
+            Console.WriteLine(1+"testando" +2+3);
+            Console.WriteLine("testando" + 1 + 2 + 3);
+            //double a = (double)5 / 2;
+            //double b = ((double)5 / (double)2);
+            Math.Pow(a, b);
+
+        }
     }
 }
