4217d8063a421aef6ad69d5af914f1751775685c
Antoni05634 <antoniogsmac@gmail.com>
2014-06-18 10:13:08 -0300

    12

--- Unidade6/Banco de Dados/Insert_Fun.sql
+++ Unidade6/Banco de Dados/Insert_Fun.sql
@@ -1,2 +1,3 @@
 ﻿select * from Funcionario order by Nome
 select * from Funcionario where salario >0 order by Deparmento_id
+select * from Funcionario where salario between 500 and 1.500 order by Nome
