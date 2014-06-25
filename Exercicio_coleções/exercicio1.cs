6d2d323a77f911e4ca87f766d0d38174eb53e341
Antoni05634 <antoniogsmac@gmail.com>
2014-06-25 11:37:12 -0300

    hsuahsu

---
+++ Exercicio_coleções/exercicio1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Exercicio_coleções
+{
+    class Program
+    {
+        public static ArrayList _ArrayList = new ArrayList();
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <100; i++)
+            {
+                _ArrayList.Add(i);
+                _ArrayList.RemoveAt(1);
+                _ArrayList.Sort();//crescente
+                 _ArrayList.Reverse(); //decrescente
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio_coleções/exercicio2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_coleções
+{
+    class exercicio2
+    {
+        static void Main7(string[] args)
+        {
+
+        }
+    }
+}
