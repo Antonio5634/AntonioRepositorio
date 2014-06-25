9a36226b11ddcfeec872a0aa6968c9da90fd6c99
Antoni05634 <antoniogsmac@gmail.com>
2014-06-25 10:34:20 -0300

    arrayList exemplos

---
+++ Unidade6/Banco de Dados/TAB_escolar.sql
@@ -0,0 +1,17 @@
+﻿-- questao 1
+--questao 2
+--SELECT A.Nome, C.Curso from TBAluno as A inner join TBCurso as C on A.TBCurso_id = C.Id order by Curso
+SELECT A.Nome, C.Nome_curso from TBAluno as A inner join TBCurso as C on A.TBCurso_id = C.id Order By TBMatricula
+-- questao 3
+select Email from TBAluno where DataNascimento >  '01/01/1995' 
+-- questao 4
+select count(*) from  TBAluno
+-- questao 5
+-- questao 6
+select Nome from TBAluno where DataNascimento <= '01/01/1996'
+-- questao 7
+select Nome from TBAluno where Sexo = 'F'
+-- questao 8
+-- questao 9
+select Nome from TBCurso order by Nome
+-- questao 10
---
+++ Unidade6/exemplo_array/Exemplos/exem1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Unidade6.exemplo_array.Exemplos
+{
+    class exem1
+    {
+        public static ArrayList _ArrayList = new ArrayList(1);
+        public static List<int> _ListInt = new List<int>();
+        public static IList<string> _IListInt = new List<string>();
+        public static Dictionary<int, string> _Dictionary = new Dictionary<int, string>();
+        static void Main5685(string[] args)
+        {
+            
+            //ARRAYLIST
+
+            //_ArrayList.Add(1);
+            //_ArrayList.Add("Antonio");
+            //_ArrayList.Add(1.85);
+            //_ArrayList.Add('M');
+            ////Remover
+            //_ArrayList.Remove(1); //remove por objeto
+            //_ArrayList.Insert(0, 1); //insere a posição
+            // _ArrayList.IndexOf(1);
+            // _ArrayList.RemoveAt(3);
+            // //_ArrayList[1] = 1 + 1;
+            // _ArrayList.Sort();//crescente
+            // _ArrayList.Reverse(); //decrescente
+            // _ArrayList.Clear(); //limpa a lista
+
+            //LISTINT
+
+            //adicionar
+
+            _ListInt.Add(12);
+            _ListInt.Add(9);
+            _ListInt.Add(6);
+            _ListInt.Add(3);
+            _ListInt.Add(15);
+            _ListInt.Add(18);
+            _ListInt.Add(21);
+            _ListInt.Add(24);
+            
+            //DiCTINARY
+
+
+
+
+            double media = _ListInt.Max();
+
+
+            
+
+
+            
+            
+            
+            
+            
+            
+            //_Dictionary.Add("a, 2");
+            //_Dictionary.Add("a, 2");
+            //_Dictionary.Add("a, 2");
+            //_Dictionary.Add("a, 2");
+            //_Dictionary.Add("a, 2");
+
+
+
+
+
+
+
+        }
+    }
+}
